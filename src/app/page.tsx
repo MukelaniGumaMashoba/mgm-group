e95cc5541e2daba33d9e0edc4d018b0aee805da0
Mukelani Guma Mashoba <mukelanilastborn@gmail.com>
2024-07-21 22:18:47 +0200

    NEXT/IMAGE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
                 <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
               </DisclosureButton>
             </div>
-            <div className="flex flex-row justify-evenly items-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-row items-center justify-evenly sm:items-stretch sm:justify-start">
 
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
@@ -61,10 +61,12 @@ export default function Home() {
             </div>
 
             <div className="flex flex-shrink-0 items-center">
-              <img
+              <Image
                 alt="mgm"
-                src="../images/logo.png"
-                className="h-20 w-auto bg-white rounded-3xl"
+                src="/images/logo.png"
+                width={60}
+                height={80}
+                className="rounded-3xl bg-white"
               />
             </div>
 
@@ -98,8 +100,8 @@ export default function Home() {
         </DisclosurePanel>
       </Disclosure>
 
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-[black] to-[#15162c] text-white">
-        <div className="p-4 rounded-md">
+      <main className="flex min-h-screen flex-col items-center justify-between bg-gradient-to-b from-[black] to-[#15162c] p-24 text-white">
+        <div className="rounded-md p-4">
           <h5 className="text-3xl font-semibold">mgm group</h5>
         </div>
 
@@ -109,7 +111,7 @@ export default function Home() {
           </SheetTrigger>
         </div>
 
-        <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        <div className="before:bg-gradient-radial after:bg-gradient-conic relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
             src="/next.svg"
