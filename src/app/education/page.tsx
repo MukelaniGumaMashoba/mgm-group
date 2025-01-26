b402e9443376a715ad84e908ac058a9b0c84a894
Mukelani Mashoba <mukelanilastborn@gmail.com>
2025-01-26 02:00:03 +0200

    Added new folders for education,team,project page

---
+++ src/app/education/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function education() {
+  return (
+    <div>
+      <h2>MGM Education</h2>
+      <div>
+        <p>Education as they say it the key to success, As MgM we also provide additional paid services to student to help them to ace their subjects
+          and also make it in future 
+        </p>
+      </div>
+    </div>
+  )
+}
