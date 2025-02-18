a70aa32a735955f8d994cccc661f4cfa68f7dbf3
Mukelani Mashoba <mukelanilastborn@gmail.com>
2025-02-18 02:00:46 +0200

    prod purpose , was suppose to use new branch but flooped pushed to main instead of new branch

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,18 +1,21 @@
+"use client"
+
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export default function Education() {
+
+  function back() {
+    window.history.back()
+  }
+
   return (
-    <div className="p-6 bg-gray-100 min-h-screen">
-      <h2 className="text-3xl font-bold mb-4 text-center">MGM Education</h2>
-      <div className="bg-white p-6 rounded-lg shadow-md">
-        <p className="mb-4 text-gray-700">
-          Education as they say is the key to success. At MGM, we also provide additional paid services to students to help them ace their subjects
-          and succeed in the future.
-        </p>
-        <p className="text-gray-700">
-          We live by the Triple C rule (Communication, Collaboration, and Creativity).
-        </p>
+    <div className="flex items-center justify-center min-h-screen flex-col bg-gray-100">
+      <div>
+        <Button onClick={back} className=''>Back</Button>
       </div>
+
+      <h1 className="text-4xl font-bold text-gray-800">Coming Soon</h1>
     </div>
-  );
+  )
 }
