e814723589d6f5ac058e8b6820850287e983fb41
Mukelani Mashoba <mukelanilastborn@gmail.com>
2025-05-02 05:47:10 +0200

    Change for teams page and projects done by mgm

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <h1>Meet Our Team</h1>
+      <div>
+        <h1 className="text-4xl font-bold text-gray-800">Our Team</h1>
+        <p className="text-gray-600">We are a team of dedicated professionals committed to delivering the best services.</p>
+      </div>
+
+      <div>
+        <div>
+          {/* <Image src="/images/edu.jpg" alt="Education" width={500} height={500} /> */}
+          <p>name</p>
+          <p>description or Bio</p>
+        </div>
+        <div>
+          {/* <Image src="/images/edu.jpg" alt="Education" width={500} height={500} /> */}
+          <p>name</p>
+          <p>description or Bio</p>
+        </div>
+      </div>
+    </div>
   )
 }
